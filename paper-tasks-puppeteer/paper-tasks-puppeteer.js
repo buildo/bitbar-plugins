282a88030784417e8841f815f65faca687052c06
Eric Camellini <eric.camellini@buildo.io>
2018-02-03 10:40:09 +0100

    Adding checkbox icon

--- paper-tasks-puppeteer/paper-tasks-puppeteer.js
+++ paper-tasks-puppeteer/paper-tasks-puppeteer.js
@@ -40,7 +40,7 @@ async function getPaperTasksCount() {
 }
 
 function onSuccess(tasksCount) {
-  console.log(tasksCount)
+  console.log(`✓ ${tasksCount}`)
   console.log('---')
   console.log('Open in browser | href=https://paper.dropbox.com/tasks')
   browser.close()
