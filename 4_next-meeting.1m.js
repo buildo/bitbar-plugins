e61865afe6086c7f0a439d4703e5cbc6df1d02ad
Francesco Cioria <firefelix@gmail.com>
2018-03-16 00:49:20 +0100

    improve next-meeting script

--- 4_next-meeting.1m.js
+++ 4_next-meeting.1m.js
@@ -10,37 +10,43 @@ const nowDate = new Date();
 const now = nowDate.toISOString();
 
 const icon = execSync('/usr/bin/curl -sL "https://cdn3.iconfinder.com/data/icons/eightyshades/512/39_Calendar-16.png" | base64')
-const agenda = execSync(`/usr/local/bin/gcalcli --calendar ${calendar} agenda ${now} --nocolor`, { encoding: 'utf8' }).trim();
+const agenda = execSync(`/usr/local/bin/gcalcli --calendar ${calendar} agenda ${now} --tsv --nocolor`, { encoding: 'utf8' }).trim();
 
-const parseLine = line => ({
-  date: line.slice(0, 10).trim(),
-  time: line.slice(12, 19).trim(),
-  event: line.slice(20).trim()
-});
+const parseLine = line => {
+  const [startDate, startTime, endDate, endTime, event] = line.split('\t');
+  return {
+    startDate, startTime, endDate, endTime, event
+  }
+}
 
-const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-const months = ['Gen', 'Feb', 'Mar', 'Apr', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+const days = ['Dom', 'Lun', 'Mar', 'Mer', 'Gio', 'Ven', 'Sab'];
+// const months = ['Gen', 'Feb', 'Mar', 'Apr', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 const pad = n => n < 10 ? `0${n}` : String(n);
-const today = `${days[nowDate.getDay()]} ${months[nowDate.getMonth()]} ${pad(nowDate.getDate())}`
+const formatDate = (isoDate) => (
+  `${days[new Date(isoDate).getDay()]} ${pad(new Date(isoDate).getDate())}` // ${months[new Date(isoDate).getMonth()]}`
+)
+
+const today = `${nowDate.getFullYear()}-${pad(nowDate.getMonth() + 1)}-${pad(nowDate.getDate())}`
 const tomorrowDate = new Date((new Date()).setDate((new Date()).getDate() + 1));
-const tomorrow = `${days[tomorrowDate.getDay()]} ${months[tomorrowDate.getMonth()]} ${pad(tomorrowDate.getDate())}`
+const tomorrow = `${tomorrowDate.getFullYear()}-${pad(tomorrowDate.getMonth() + 1)}-${pad(tomorrowDate.getDate())}`
 
-const lines = agenda.split('\n').filter(line => line.trim());
+const lines = agenda.split('\n').map((line => parseLine(line.trim())));
 
-const numberOfEventsToday = lines.reduce((acc, line, index) => parseLine(line).date && parseLine(line).date !== today && acc === null ? index : acc, null);
+const numberOfEventsToday = lines.filter(line => line.startDate === today).length;
 
 console.log(`${numberOfEventsToday} | image=${icon}\n---`);
 
-const agendaParsed = lines.forEach((line, index) => {
-  const { date, time, event } = parseLine(line)
+const agendaParsed = lines.forEach(({ startDate, startTime, endTime, event }, index) => {
 
   // if (index === 0) {
     // console.log(`${time} ${event.length > 15 ? event.slice(0, 12).trim() + '...' : event} | image=${icon}`)
     // console.log('---')
   // }
 
-  date && console.log(`${date === today ? 'Today' : date === tomorrow ? 'Tomorrow' : date} | color=#848484`)
-  console.log(`    ${time} ${event} | href=https://calendar.google.com trim=false`)
+  if (startDate !== (lines[index - 1] || {}).startDate) {
+    console.log(`${startDate === today ? 'Oggi' : startDate === tomorrow ? 'Domani' : formatDate(startDate)} | color=#848484`)
+  }
+  console.log(`    ${startTime}-${endTime} ${event} | href=https://calendar.google.com trim=false`)
 });
 
 console.log('---')
