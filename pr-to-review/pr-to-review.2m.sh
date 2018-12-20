fd0a5657db95e99db1da5c3089d7762fa2f59072
Gabriele Petronella <gabriele@buildo.io>
2018-12-20 15:46:56 +0100

    Remove GHE support

--- pr-to-review/pr-to-review.2m.sh
+++ pr-to-review/pr-to-review.2m.sh
@@ -3,31 +3,24 @@
 ######### CONFIGURATION #########
 GITHUB_USER="" # e.g. "gabro"
 GITHUB_COM_TOKEN="" # get one at https://github.com/settings/tokens
-GITHUB_ENTERPRISE_TOKEN="" # get one at https://github.omnilab.our.buildo.io/settings/tokens
 #################################
 
 PATH=$PATH:/usr/local/bin
 
 GITHUB_COM_API="https://api.github.com"
-GITHUB_ENTERPRISE_API="https://github.omnilab.our.buildo.io/api/v3"
 
 QUERY="review-requested:$GITHUB_USER+type:pr+state:open"
 
 GITHUB_COM_RESULT=`curl -ks $GITHUB_COM_API/search/issues\?q\=$QUERY\&access_token\=$GITHUB_COM_TOKEN`
-GITHUB_ENTERPRISE_RESULT=`curl -ks $GITHUB_ENTERPRISE_API/search/issues\?q\=$QUERY\&access_token\=$GITHUB_ENTERPRISE_TOKEN`
 
-GITHUB_COM_COUNT=`echo $GITHUB_COM_RESULT | jq '.total_count'`
-GITHUB_ENTERPRISE_COUNT=`echo $GITHUB_ENTERPRISE_RESULT | jq '.total_count'`
-
-PR_COUNT=$(($GITHUB_COM_COUNT + $GITHUB_ENTERPRISE_COUNT))
+PR_COUNT=`echo $GITHUB_COM_RESULT | jq '.total_count'`
 
 function colorForCount {
-  if [ $1 = 0 ]; then echo 'green'; else echo 'red'; fi
+  if [ $PR_COUNT = 0 ]; then echo 'green'; else echo 'red'; fi
 }
 
 COLOR=`colorForCount $PR_COUNT`
 GITHUB_COM_COLOR=`colorForCount $GITHUB_COM_COUNT`
-GITHUB_ENTERPRISE_COLOR=`colorForCount $GITHUB_ENTERPRISE_COUNT`
 
 case $BitBarDarkMode in
   # 1) ICON_URL=https://s29.postimg.org/8znywfcd3/review_icon_13.png ;;
@@ -36,7 +29,4 @@ esac
 
 MERGE_ICON=`curl -sL $ICON_URL | base64`
 
-echo " $PR_COUNT | color=$COLOR image=$MERGE_ICON"
-echo "---"
-echo "github.com: $GITHUB_COM_COUNT | href=https://github.com/pulls/review-requested color=$GITHUB_COM_COLOR"
-echo "omnilab: $GITHUB_ENTERPRISE_COUNT | href=https://github.omnilab.our.buildo.io/pulls/review-requested color=$GITHUB_ENTERPRISE_COLOR"
+echo " $PR_COUNT | color=$COLOR image=$MERGE_ICON href=https://github.com/pulls/review-requested color=$GITHUB_COM_COLOR"
