dc285b0d87c72c92896b21ca9e9d4d0f257cb2a3
Riccardo Ancona <riccardo.ancona@buildo.io>
2021-01-22 09:40:56 +0100

    Changed user identification with email

--- kaiten/kaiten.2m.sh
+++ kaiten/kaiten.2m.sh
@@ -1,22 +1,22 @@
 #! /bin/bash
 
 ######### CONFIGURATION #########
-KAITEN_USER="" # e.g. "12345"
 KAITEN_TOKEN="" # get one at https://buildo.kaiten.io/profile/api-key
+KAITEN_EMAIL="" # e.g. riccardo.ancona@buildo.io
 #################################
 
 PATH=$PATH:/usr/local/bin
 
 KAITEN_API="https://buildo.kaiten.io/api/v1"
-
+KAITEN_USER=`curl -s --request GET $KAITEN_API/users/ --header "Authorization: Bearer $KAITEN_TOKEN" | jq '.[] | select(.email == "'$KAITEN_EMAIL'") | .id'`
 KAITEN_COM_RESULT=`curl -s --request GET $KAITEN_API/users/$KAITEN_USER/cards --header "Authorization: Bearer $KAITEN_TOKEN"`
 
 
 KAITEN_MEMBERS_QUERY=".[] | select((.column.type == 1 or .column.type ==2) and has(\"members\")) | .members[] | select(.user_id == $KAITEN_USER and .type == 1)"
 KAITEN_RESPONSIBLE_QUERY=".[] | select((.column.type == 1 or .column.type ==2) and has(\"members\")) | .members[] | select(.user_id == $KAITEN_USER and .type == 2)"
 
-KAITEN_MEMBERS_COUNT=`echo $KAITEN_COM_RESULT | jq -c "$KAITEN_MEMBERS_QUERY" | wc -l`
-KAITEN_RESPONSIBLE_COUNT=`echo $KAITEN_COM_RESULT | jq -c "$KAITEN_RESPONSIBLE_QUERY" | wc -l`
+KAITEN_MEMBERS_COUNT=$((`echo $KAITEN_COM_RESULT | jq -c "$KAITEN_MEMBERS_QUERY" | wc -l`+0))
+KAITEN_RESPONSIBLE_COUNT=$((`echo $KAITEN_COM_RESULT | jq -c "$KAITEN_RESPONSIBLE_QUERY" | wc -l`+0))
 
 COUNT=$(($KAITEN_MEMBERS_COUNT+$KAITEN_RESPONSIBLE_COUNT))
 
@@ -27,7 +27,7 @@ function colorForCount {
 
 COLOR=`colorForCount $COUNT`
 
-echo "⛩ $COUNT"
+echo "⛩ $COUNT ($KAITEN_RESPONSIBLE_COUNT/$KAITEN_MEMBERS_COUNT)"
 echo "---"
 echo "Responsible of: $KAITEN_RESPONSIBLE_COUNT cards | href=https://buildo.kaiten.io/dashboard/cards?member=0&responsible=1 color=$COLOR"
 echo "Member of: $KAITEN_MEMBERS_COUNT cards| href=https://buildo.kaiten.io/dashboard/cards?member=1&responsible=0 color=$COLOR"
